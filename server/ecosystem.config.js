35dc7b7d61e9e8d6eb3a3018d43782d28c5a560c
gjasay <gabeasay@gmail.com>
2025-02-02 07:26:31 -0500

    revert

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,4 +1,4 @@
-const os = require("os");
+const os = require('os');
 
 /**
  * COLYSEUS CLOUD WARNING:
@@ -7,20 +7,17 @@ const os = require("os");
  */
 
 module.exports = {
-  apps: [
-    {
-      name: "colyseus-app",
-      script: "build/index.js",
-      time: true,
-      watch: false,
-      instances: os.cpus().length,
-      exec_mode: "fork",
-      wait_ready: true,
-      env_production: {
-        NODE_ENV: "production",
-      },
-      post_deploy:
-        "curl -fsSL https://bun.sh/install | bash && export PATH=$HOME/.bun/bin:$PATH && bun install && bun run build && npm exec colyseus-post-deploy",
-    },
-  ],
+  apps : [{
+    name: "colyseus-app",
+    script: 'build/index.js',
+    time: true,
+    watch: false,
+    instances: os.cpus().length,
+    exec_mode: 'fork',
+    wait_ready: true,
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
 };
+
